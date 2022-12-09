6c9901d4e9f25ddd871ad79ff87413b98a1597b2
Julian Atkin <jatkindev@gmail.com>
2022-12-09 09:44:19 -0500

    Test serverless update

--- api/crop.ts
+++ api/crop.ts
@@ -4,11 +4,13 @@ import sharp from 'sharp';
 
 export default function handler (req: VercelRequest, res: VercelResponse) {
     const { dataURL, x, y, w, h } = req.query;   
-    const b64 = trimDataURL(dataURL);
-    const image = sharp(b64);
-    image.resize({width: 100})
-    .then(output => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.send(output)
-    });
+    res.send(dataURL);
+
+    // const b64 = trimDataURL(dataURL);
+    // const image = sharp(b64);
+    // image.resize({width: 100})
+    // .then(output => {
+    //     res.setHeader('Access-Control-Allow-Origin', '*');
+    //     res.send(output)
+    // });
 };
